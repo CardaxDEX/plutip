710b37f3be3fe89f793c8e20a96fb6a3b114c838
Andrea Vezzosi <andrea@mlabs.city>
2022-05-13 13:26:38 +0200

    use hacked-budget bpi

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
       flake = false;
     };
     bot-plutus-interface.url =
-      "github:mlabs-haskell/bot-plutus-interface?rev=9ca5939b3b6ef1d052318f25ac88444baea52d8b";
+      "github:CardaxDEX/bot-plutus-interface?ref=saizan/hacked-budget";
   };
 
   outputs =
